e5ef4a159f561ba805bbfa087a2a4ca125bfb652
mohamed <137658373+alarabyy@users.noreply.github.com>
2024-08-10 22:16:25 +0300

    Add files via upload

---
+++ 6_publes_sheet/js/main.js
@@ -0,0 +1,15 @@
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+let isDarkMode = false;
+
+darkModeToggle.addEventListener('click', () => {
+    isDarkMode = !isDarkMode;
+    if (isDarkMode) {
+        body.classList.add('dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
+    }
+});
+
+// ----------------------------------------------------------------------------------

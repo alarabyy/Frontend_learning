be04e8dfc9ce6567090e988b6be0424c675fd56d
alarabyy <137658373+alarabyy@users.noreply.github.com>
2024-08-27 03:20:31 +0300

    add fils

---
+++ HTML ,CSS ,SASS ,JS ,BS ,TW -Fundamentals/10_Chatbot/js/script.js
@@ -0,0 +1,52 @@
+const messageBar = document.querySelector(".bar-wrapper input");
+const sendBtn = document.querySelector(".bar-wrapper button");
+const messageBox = document.querySelector(".message-box");
+
+const API_URL = "https://api.openai.com/v1/chat/completions";
+const API_KEY = "sk-None-LkbMTEOvJMryQS08OlBjT3BlbkFJqtOLaWeE4b08NBIoO2gC";
+
+sendBtn.onclick = function () {
+  if (messageBar.value.length > 0) {
+    const UserTypedMessage = messageBar.value;
+    messageBar.value = "";
+
+    let message =
+      `<div class="chat message">
+      <img src="img/user.jpg">
+      <span>${UserTypedMessage}</span>
+    </div>`;
+
+    let response =
+      `<div class="chat response">
+      <img src="img/chatbot.jpg">
+      <span class="new">...</span>
+    </div>`;
+
+    messageBox.insertAdjacentHTML("beforeend", message);
+    messageBox.insertAdjacentHTML("beforeend", response);
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${API_KEY}`
+      },
+      body: JSON.stringify({
+        "model": "gpt-3.5-turbo",
+        "messages": [{ "role": "user", "content": UserTypedMessage }]
+      })
+    };
+
+    fetch(API_URL, requestOptions)
+      .then(res => res.json())
+      .then(data => {
+        const ChatBotResponse = document.querySelector(".response .new");
+        ChatBotResponse.innerHTML = data.choices[0].message.content;
+        ChatBotResponse.classList.remove("new");
+      })
+      .catch((error) => {
+        const ChatBotResponse = document.querySelector(".response .new");
+        ChatBotResponse.innerHTML = "Oops! An error occurred. Please try again.";
+      });
+  }
+}
